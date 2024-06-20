6d8d23121088a553ccfb0330b39c9b242ea287a0
Tibor Lapos <lapos@aconsulting.sk>
2024-06-20 12:59:47 +0200

    enable cors policy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: true, // Allows all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Teso Price Analyzer')
     .setVersion('1.0')
